56aa7b11ff230b651f00de2788eef48de6ecfd15
ryc-o <rytel.mateusz@wp.pl>
2021-12-09 13:01:39 +0100

    Initial commit

---
+++ src/day1/Solution.java
@@ -0,0 +1,20 @@
+package day1;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/day1/input.txt"));
+        int counter = 0;
+        int firstValue = Integer.parseInt(reader.readLine());
+        while(reader.ready()){
+            int secondValue = Integer.parseInt(reader.readLine());
+            if(secondValue > firstValue)
+                counter++;
+            firstValue = secondValue;
+        }
+        System.out.print(counter);
+    }
+}
